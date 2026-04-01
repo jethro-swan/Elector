0ff18e9d470cd42929478bc871df2b2e0d029bd7
jethro-swan <john.waters@cooptel.net>
2026-04-01 22:41:20 +0100

    gggggg

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hmmm???"
+    candidates = {
+        "1": "Henry Crun",
+        "2": "Dennis Bloodnok",
+        "3": "Minnie Bannister"
+    }
+    return render_template(
+        "index.html", title="AGM 2026", candidates=candidates
+    )
