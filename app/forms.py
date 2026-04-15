eeb30ef8b7821a48667f3c18d24a697438082e17
jethro-swan <john.waters@cooptel.net>
2026-04-15 01:02:48 +0100

    Mostly ready

--- app/forms.py
+++ app/forms.py
@@ -3,52 +3,26 @@ import sys
 # Flask components: -----------------------------------------------------------
 
 from flask_wtf import FlaskForm, RecaptchaField
-#from flask_wtf import Form ###
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-#from wtforms import TextField, TextAreaField, SelectField, RadioField
 from wtforms import TextAreaField, SelectField, RadioField, HiddenField
-from wtforms import IntegerField, DecimalField, FloatField
-from wtforms import DateField, DateTimeField
 from wtforms.validators import DataRequired, InputRequired, Email
-from wtforms.validators import Length, EqualTo
-from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 #==============================================================================
 
 
-
-
-
-
-#------------------------------------------------------------------------------
-
 class VoteForm(FlaskForm):
 
-    for candidate in candidates:
+    email       = StringField(
+                    "email address",
+                    validators=[DataRequired("required"), Email()]
+                  )
 
-        
-    category        = RadioField(
-                        "Category",
-                        choices = [
-                                    ("payment_received", "payment received"),
-                                    ("offer", "offer"),
-                                    ("request", "request"),
-                                    ("payment_request", "payment request"),
-                                    ("event", "event"),
-                                    ("other", "other")
-                                  ],
-                        default = "other"
-                      )
+    password    = PasswordField(
+                    "password",
+                    #render_kw={"autocomplete": "on"},
+                    validators=[DataRequired("required")]
+                  )
 
-
-    vote    = SubmitField("vote")
-
-#------------------------------------------------------------------------------
-
-class LoginForm(FlaskForm):
-
-    email           = StringField("email address")
-
-    submit          = SubmitField("log in")
+    vote        = SubmitField("vote")
 
 #------------------------------------------------------------------------------
--- app/routes.py
+++ app/routes.py
@@ -2,56 +2,80 @@
 from flask import render_template, render_template_string
 from flask import flash, redirect, url_for
 from flask import session, g, request
-#from flask import Flask, session, g, request
-#from flask_mailman import Mail, EmailMessage
-#from flask_mailman import EmailMessage
-from flask_login import LoginManager, current_user, login_user, logout_user
-from flask_login import login_required
-from flask import send_file
-#from flask import send_from_directory
 from app import app
 
-# log in to vote --------------------------------------------------------------
-@app.route("/", methods = ["GET", "POST"])
-@app.route("/login", methods = ["GET", "POST"])
-def login():
-    if current_user.is_authenticated: # user is already logged in
-        return redirect(url_for("vote"))
-    form = LoginForm()
-    if form.validate_on_submit():
-        password = form.password.data
-        if password == "nopasaran":
-            return redirect(url_for("vote"))
+from app.core.constants import VOTERS_DB
+from app.core.constants import MEMBERS_LIST
+from app.core.constants import CANDIDATES_LIST
+from app.core.constants import VOTES_LOG
+
+
+@app.route('/read_vote_form', methods=['POST'])
+def read_vote_form():
+
+    # Get the form data as Python ImmutableDict datatype
+    data = request.form
+
+    email = data['userEmail']
+    # INSERT code to check that email is that of a member
+
+    password = data['userPassword']
+    if password != "nopasaran":
+        flash("Invaid password")
+        return redirect(url_for("invalid_password"))
+
+    # INSERT code to store results in database and log
+
+    for k in data.keys():
+        print(k + " :: " + data[k])
+
+    flash("Your vote has been recorded")
+    return redirect(url_for("vote_recorded"))
+
+
+@app.route("/vote_recorded")
+def vote_recorded():
     return render_template(
-        "login.html",
-        title = "Sign in",
-        form = form
+        "vote_recorded.html",
+        title="vote recorded"
+    )
+
+@app.route("/invalid_voter")
+def invalid_voter():
+    return render_template(
+        "invalid_voter.html",
+        title="invalid voter"
+    )
+
+@app.route("/invalid_password")
+def invalid_password():
+    return render_template(
+        "invalid_password.html",
+        title="invalid password"
     )
 
 
-# log out ---------------------------------------------------------------------
-@app.route("/logout")
-@login_required
-def logout():
-    user = current_user.get_id()
-    logout_user() # a Flask function
-    return redirect(url_for("login"))
-
 # vote ------------------------------------------------------------------------
-@app.route("/vote")
-@login_required
+@app.route("/", methods = ["GET"])
+@app.route("/vote", methods = ["GET"])
 def vote():
+    with open(CANDIDATES_LIST, "r") as cl:
+        candidates_list = cl.readlines()
     candidates = []
-    with open("/var/elector/candidates.txt", "r") as cl:
-        candidates = cl.readlines()
-    # TESTSTUFF
-    for c in candidates:
-        print(c)
-    ###########
-    if form.validate_on_submit():
-        print("ZZZZZ")
+    n_candidates = len(candidates_list)
+    for c_num in range(n_candidates):
+        candidate = {}
+        candidate["id"] = str(c_num)
+        candidate["name"] = candidates_list[c_num].strip()
+        candidates.append(candidate)
 
+    arg_req = []
+    for c_num in range(n_candidates):
+        arg_req.append(str(c_num) + "_vote")
 
+    print(candidates)
+    for c_num in range(n_candidates):
+        arg_req.append(str(c_num) + "_vote")
 
     return render_template(
         "vote.html",
