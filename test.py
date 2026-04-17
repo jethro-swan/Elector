410c8d6b571cc55b5ba6f8d8ef1886c085da7934
jethro-swan <john.waters@cooptel.net>
2026-04-17 01:06:39 +0100

    --

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/home/john/Sync/Elector/venv/bin/python3
-#!/home/elector/ELECTOR/venv/bin/python3
-
-from app.core.login import member_exists
-#from app.core.login import register_authenticated_login
-#from app.core.login import deregister_authenticated_login
-from app.core.login import check_authenticated_login
-
-test_user = "john.waters@cooptel.net"
